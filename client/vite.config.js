5f7b95a2d95396584289e8145aaefc25002c77e3
Caio <caiofortesprofissional@gmal.com>
2024-06-12 22:56:54 -0300

    testando alterações de deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // Remove o prefixo /api
+export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    return {
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '')
+          }
+        }
       }
-    }
+    };
   }
+  
+  // Configurações para outros ambientes (produção, etc.)
+  return {};
 });
